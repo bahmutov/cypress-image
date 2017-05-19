f92dd21a1d4f6ee7fcf345401ec52de94c92c811
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-05-19 14:01:17 -0400

    upgrade to Cypress 0.19.2

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,6 +1,8 @@
 set e+x
 
 # build image with everything needed to run Cypress
-VERSION=0.16.4
-LOCAL_NAME=front-end/cypress-ci
+VERSION=0.19.2
+LOCAL_NAME=bahmutov/cypress-image
+
+echo "Building $LOCAL_NAME with Cypress $VERSION"
 docker build --build-arg CYPRESS_VERSION=${VERSION} -t $LOCAL_NAME .
